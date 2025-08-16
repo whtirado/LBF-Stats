c7f90e2b34f24ab229d2ef010606dac1d028cd3c
William Tirado <whtirado@gmail.com>
2025-08-16 11:47:28 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import { discordChannelID } from "./src/config.js";
+import { Client, GatewayIntentBits, TextChannel } from "discord.js";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+client.once("ready", async () => {
+  console.log(`Bot is online: LBF Stats`);
+  const channel = await client.channels.fetch(discordChannelID);
+
+  if (channel && channel.isTextBased()) {
+    (channel as TextChannel).send("Hello from the LBF Stats!");
+  } else {
+    console.error("Channel not found or is not a text channel.");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,6 @@
+// Database
 export const playerSteamIDsPath = ""
+
+// Discord
+export const discordGuildID = "609577058661695489"
+export const discordChannelID = "1401515759397638164"
